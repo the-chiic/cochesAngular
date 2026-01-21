9a36ff45501806372fa691c6aa498e85233358a0
tilin <adririsco05@gmail.com>
2026-01-21 21:02:52 +0100

    Modificar y Borrar Modificacion2

--- src/app/components/coche/coche.component.ts
+++ src/app/components/coche/coche.component.ts
@@ -9,11 +9,52 @@ import { ServiceCochesService, Coche } from '../../services/service-coches.servi
 export class CocheComponent implements OnInit {
 
   coches: Coche[] = [];
+  marca: string = "";
+  modelo: string = "";
+  ano: number = 0;
+  imagen: string = "";
+
+  editMode: boolean = false;
+  editId: number | null = null;
 
   constructor(private cocheService: ServiceCochesService) { }
 
   ngOnInit(): void {
     this.coches = this.cocheService.getCoches();
+  }
+
+  addCoche(){
+    if(this.marca.length > 2 && this.modelo.length > 2 && this.ano > 1900){
+      if(this.editMode && this.editId !== null){
+        this.cocheService.updateCoche({id: this.editId, marca: this.marca, modelo: this.modelo, ano: this.ano, imagen: this.imagen});
+        this.editMode = false;
+        this.editId = null;
+      } else {
+        this.cocheService.addCoche(this.marca, this.modelo, this.ano, this.imagen);
+      }
+      this.resetForm();
+    }
+  }
+
+  editCoche(coche: Coche){
+    this.editMode = true;
+    this.editId = coche.id;
+    this.marca = coche.marca;
+    this.modelo = coche.modelo;
+    this.ano = coche.ano;
+    this.imagen = coche.imagen;
+  }
+
+  deleteCoche(id: number){
+    this.cocheService.deleteCoche(id);
+    this.coches = this.cocheService.getCoches(); // 🔄 refrescamos la lista
+  }
+
+  resetForm(){
+    this.marca = "";
+    this.modelo = "";
+    this.ano = 0;
+    this.imagen = "";
   }
 
 }
--- src/app/services/service-coches.service.ts
+++ src/app/services/service-coches.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 
 export interface Coche {
+  id: number;
   marca: string;
   modelo: string;
   ano: number;
@@ -13,21 +14,38 @@ export interface Coche {
 export class ServiceCochesService {
 
   private coches: Coche[] = [
-    { marca: 'Rayo', modelo: 'McQueen', ano: 2005, imagen: 'assets/img/coche1.png' },
-    { marca: 'BMW', modelo: 'Serie 3', ano: 2021, imagen: 'assets/img/coche2.png' },
-    { marca: 'Audi', modelo: 'A4', ano: 2022, imagen: 'assets/img/coche3.png' },
-    { marca: 'Ford', modelo: 'Mustang', ano: 2019, imagen: 'assets/img/coche4.png' },
-    { marca: 'Honda', modelo: 'Civic', ano: 2018, imagen: 'assets/img/coche5.png' },
-    { marca: 'Mercedes', modelo: 'C-Class', ano: 2021, imagen: 'assets/img/coche6.png' },
-    { marca: 'Bugatti', modelo: 'Gold', ano: 2020, imagen: 'assets/img/coche7.png' },
-    { marca: 'Tesla', modelo: 'Model 3', ano: 2022, imagen: 'assets/img/coche8.png' },
-    { marca: 'Fernando', modelo: 'Alonso', ano: 2022, imagen: 'assets/img/coche9.png' },
-    { marca: 'Policía', modelo: 'Nacional', ano: 2021, imagen: 'assets/img/coche10.png' }
+    { id:1, marca: 'Rayo', modelo: 'McQueen', ano: 2005, imagen: 'assets/img/coche1.png' },
+    { id:2, marca: 'BMW', modelo: 'Serie 3', ano: 2021, imagen: 'assets/img/coche2.png' },
+    { id:3, marca: 'Audi', modelo: 'A4', ano: 2022, imagen: 'assets/img/coche3.png' },
+    { id:4, marca: 'Ford', modelo: 'Mustang', ano: 2019, imagen: 'assets/img/coche4.png' },
+    { id:5, marca: 'Honda', modelo: 'Civic', ano: 2018, imagen: 'assets/img/coche5.png' },
+    { id:6, marca: 'Mercedes', modelo: 'C-Class', ano: 2021, imagen: 'assets/img/coche6.png' },
+    { id:7, marca: 'Bugatti', modelo: 'Gold', ano: 2020, imagen: 'assets/img/coche7.png' },
+    { id:8, marca: 'Tesla', modelo: 'Model 3', ano: 2022, imagen: 'assets/img/coche8.png' },
+    { id:9, marca: 'Fernando', modelo: 'Alonso', ano: 2022, imagen: 'assets/img/coche9.png' },
+    { id:10, marca: 'Policía', modelo: 'Nacional', ano: 2021, imagen: 'assets/img/coche10.png' }
   ];
 
   constructor() { }
 
   getCoches(): Coche[] {
     return this.coches;
+  }
+
+  addCoche(marca: string, modelo: string, ano: number, imagen: string){
+    const id = this.coches.length > 0 ? Math.max(...this.coches.map(c => c.id)) + 1 : 1;
+    const coche: Coche = { id, marca, modelo, ano, imagen };
+    this.coches.push(coche);
+  }
+
+  deleteCoche(id: number){
+    this.coches = this.coches.filter(c => c.id !== id);
+  }
+
+  updateCoche(updatedCoche: Coche){
+    const index = this.coches.findIndex(c => c.id === updatedCoche.id);
+    if(index !== -1){
+      this.coches[index] = updatedCoche;
+    }
   }
 }
