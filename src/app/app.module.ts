d311257d5d927d56f8808e72ff76c8d0fde5dc1c
tilin <adririsco05@gmail.com>
2026-01-22 08:41:15 +0100

    Modificar y Borrar Modificacion3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './coche/services/app-routing.module';
 import { AppComponent } from './app.component';
 import { CocheComponent } from './components/coche/coche.component';
 import { FormsModule } from '@angular/forms';
