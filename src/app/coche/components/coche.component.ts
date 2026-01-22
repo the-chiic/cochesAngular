0fe28e8684b7e140c64f48ec712f338811ca1fc4
tilin <adririsco05@gmail.com>
2026-01-22 09:58:36 +0100

    Modificar y Borrar Modificacion5

--- src/app/coche/components/coche.component.ts
+++ src/app/coche/components/coche.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ServiceCochesService, Coche } from '../../services/service-coches.service';
+import { ServiceCochesService, Coche } from '../services/service-coches.service';
 
 @Component({
   selector: 'app-coche',
